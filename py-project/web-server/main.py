df3aaefb3cc970604e5b8c169f5e271ff0b77b52
Ximena Alcaman <flawergerl.x@gmail.com>
2023-04-09 16:16:28 -0400

    add fastAPI and uvicorn

--- py-project/web-server/main.py
+++ py-project/web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+
+
+app = FastAPI()
+
+@app.get('/') # principal route
+def get_list():
+    return [1, 2, 3, 4]
+
+@app.get('/contact', response_class=HTMLResponse) # contact route
+def get_list():
+    return '''
+        <h1> Contact </h1>
+        <p> This is a contact page </p>
+    '''
+
+
 
 
 def run():
